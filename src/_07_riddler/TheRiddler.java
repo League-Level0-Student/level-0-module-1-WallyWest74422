c4f41561ecd12cc1f291e80021097586c09bd4c4
League Student <league@iMac-22.attlocal.net>
2019-09-15 15:56:42 -0700

    ZOMBIE EYESSSS!

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -5,27 +5,39 @@ import javax.swing.JOptionPane;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
-
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-int score = 0;
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-String answerone =JOptionPane.showInputDialog("Forward, I am heavy. Backward, I am not. What am I?");
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-if(answerone.equalsIgnoreCase("ton")) {
-JOptionPane.showMessageDialog(null, "Correct!");
-score++;
-		// 5. Otherwise, say "wrong" and tell them the answer
-}else {JOptionPane.showMessageDialog(null, "Wrong. The answer was ton");
-		// 6. Add some more riddles
+		String answerone = JOptionPane.showInputDialog("Forward, I am heavy. Backward, I am not. What am I?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (answerone.equalsIgnoreCase("ton")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score++;
+			JOptionPane.showMessageDialog(null, "Your score is " + score);
+			// 5. Otherwise, say "wrong" and tell them the answer
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong. The answer was ton");
+			JOptionPane.showMessageDialog(null, "Your score is " + score);
+		}
 
-		// 2. Make a pop up to show the score.
-		
-JOptionPane.showMessageDialog(null, "Your score is " + score );}
+		// 6. Add some more riddles
+		String answertwo = JOptionPane.showInputDialog(
+				"If you have me, you'll want to share me. If you share me, you haven't got me. What am I?");
+		{
+			if (answertwo.equalsIgnoreCase(" a secret")) {
+				JOptionPane.showMessageDialog(null, "Correct!");
+				score++;
+				JOptionPane.showMessageDialog(null, "Your score is " + score);
+				// 2. Make a pop up to show the score.
+			}else {
+				JOptionPane.showMessageDialog(null, "Wrong. The answer was a secret");
+				JOptionPane.showMessageDialog(null, "Your score is " + score);
+			}
+		}
 	}
 }
-
--- src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,15 @@
 
 void setup() {
-
+PImage face = loadImage("acePic.jpg");
+size(500, 500);
+face.resize(width, height);
+image(face, 0, 0);
+noStroke();
 }
 void draw() {
+println(mouseX + "    " + mouseY);
+  
+  ellipse(224, 122, 30, 30);
+ ellipse(273, 122, 30, 30);
 
 }
